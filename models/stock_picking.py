b2265f7316973b617279b5a88f012a5829f4e314
Amara Baradji <amarabaradji@192.168.68.104>
2023-08-16 14:59:33 +0000

    Kanban view

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -7,24 +7,49 @@ class StockPickingTypeInherit(models.Model):
 	plant_id = fields.Many2one("mrp.plant", _("Plant"))
 
 	def get_mrp_stock_picking_action_picking_type(self):
-		res = super(StockPickingTypeInherit, self).get_mrp_stock_picking_action_picking_type()
-		# res['context']['search_default_by_plants'] = 1
-		print("Res", res['context'], type(res['context']))
-		if 'context' in res:
-			context = eval(res['context'])
+		# res = super(StockPickingTypeInherit, self).get_mrp_stock_picking_action_picking_type()
+		# # res['context']['search_default_by_plants'] = 1
+		# print("Res", res['context'], type(res['context']))
+		# if 'context' in res:
+		# 	context = eval(res['context'])
 
-		else:
-			context = {}
+		# else:
+		# 	context = {}
 
-		if 'default_picking_type_id' in eval(res['context']):
-			context['default_picking_type_id'] = self.id
+		# if 'default_picking_type_id' in eval(res['context']):
+		# 	context['default_picking_type_id'] = self.id
 
-		context['search_default_group_by_planning'] = 1
-		context['search_default_group_by_section'] = 1
-		context['search_default_group_by_packaging_line_id'] = 1
-		res['context'] = str(context)
-		print("Res exec")
-		return res
+		# context['search_default_group_by_planning'] = 1
+		# context['search_default_group_by_section'] = 1
+		# context['search_default_group_by_packaging_line_id'] = 1
+		# res['context'] = str(context)
+		# print("Res exec")
+
+		action = {
+			'name': "Manufacturing Orders",
+			'type': "ir.actions.act_window",
+			'res_model': "mrp.production",
+			'view_mode': "tree,kanban,form,calendar,pivot,graph",
+			'search_view_id': self.env.ref('mrp.view_mrp_production_filter').id,
+			'context': {
+				'search_default_todo': True,
+			  'default_company_id': self.env.company.id,
+
+			  # 'search_default_by_plants': 1,
+			  'search_default_group_by_planning':1,
+			  'search_default_group_by_section':1,
+			  'search_default_group_by_packaging_line_id':1,
+			},
+			'domain': [('picking_type_id.active', '=', True)],
+			'help': """
+				<p class="o_view_nocontent_smiling_face">
+	                No manufacturing order found. Let's create one.
+	              </p><p>
+	                Consume <a name="%(product.product_template_action)d" type='action' tabindex="-1">components</a> and build finished products using <a name="%(mrp_bom_form_action)d" type='action' tabindex="-1">bills of materials</a>
+	              </p>
+			"""
+		}
+		return action
 
 
 
